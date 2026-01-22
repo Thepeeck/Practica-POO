0f9b6df4f5806e98ca5d344888d36c4e2ceaeb1f
The_peeck <iguardado158@gmail.com>
2026-01-22 06:44:55 -0600

    Update Celda.java

--- App/src/Celda.java
+++ App/src/Celda.java
@@ -1,12 +1,13 @@
 public class Celda  implements InterfazCelda{
-    private boolean revelado = false;
-    private boolean marcado = false;
-    private boolean tieneMina;
+    private boolean revelado = false; // indica si la celda fue revelada 
+    private boolean marcado = false; //indica si la celda esta marcada, es deicr, marcada con una bandera 
+    private boolean tieneMina; // indica si la celda tiene una mina 
 
+    // este es constructor, que crea una celda e indica si esta esta vacia o esta tiene una mina 
     public Celda(boolean tieneMina) {
         this.tieneMina = tieneMina;
     }
-
+    // este devuelve un -1 si la celda tiene una mina, o el numero de minas cerca 
     public int siExplota(int minasCercanas) {
         if (tieneMina) {
             return -1;
@@ -14,24 +15,24 @@ public class Celda  implements InterfazCelda{
             return minasCercanas;
         }
     }
-
+// retorna si la celda contiene la mina 
     public boolean tieneMina() {
         return tieneMina;
     }
-
+// este revela la celda
     public void revelar() {
         this.revelado = true;
     }
-
+//indica si la celda fue revelada
     public boolean estaRevelado() {
         return revelado;
     }
-
+// maraca o desmarca la celda por medio de la bandera 
     public boolean marcar() {
         this.marcado = (this.marcado ? true : false);
         return marcado;
     }
-
+// este indica si la celda en efecto esta maracada 
     public boolean estaMarcado() {
         return marcado;
     }
